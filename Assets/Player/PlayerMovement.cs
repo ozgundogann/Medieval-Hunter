08d8d1e59bb5215f58ddb8afc1e05dd723277d67
Özgün DOĞAN <ozgundogan2000@gmail.com>
2023-08-09 04:13:39 +0300

    Player Enhancements are made

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -9,27 +9,45 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] FixedJoystick fixedJoystick;
     [SerializeField] float speed;
     [SerializeField] float maxSpeed = 5f;
-    
+    [SerializeField] Animator animator;
+    [SerializeField] float attackSpeed = 2f;
+
     private Vector3 addedPos;
+    private Vector3 lookVector;
     private float decreaseRate = 0.5f;
 
     float horizontal;
     float vertical;
 
-    // Start is called before the first frame update
     void Start()
     {
         speed = maxSpeed;
+        StartCoroutine(AttackFunc());
     }
 
     void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void MovePlayer()
     {
         horizontal = fixedJoystick.Horizontal;
         vertical = fixedJoystick.Vertical;
 
         addedPos = new Vector3(horizontal * speed * Time.fixedDeltaTime, 0, vertical * speed * Time.fixedDeltaTime);
 
-        transform.position += addedPos; 
+        transform.position += addedPos;
+    }
+
+    private void RotatePlayer()
+    {
+        lookVector = Vector3.forward * fixedJoystick.Vertical - Vector3.left * fixedJoystick.Horizontal;
+        if (fixedJoystick.Horizontal != 0 || fixedJoystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(lookVector * Time.fixedDeltaTime);
+        }
     }
 
     public void DecreaseSpeed()
@@ -40,5 +58,19 @@ public class PlayerMovement : MonoBehaviour
     public void IncreaseSpeed()
     {
         speed = maxSpeed;
+    }
+
+    IEnumerator AttackFunc()
+    {
+        while (true)
+        {
+            Attack();
+            yield return new WaitForSeconds(attackSpeed);
+        }
+    }
+
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
     }
 }
